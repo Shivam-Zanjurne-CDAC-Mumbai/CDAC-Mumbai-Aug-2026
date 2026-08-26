32f5a83e53fa590be540eb810b7b9e47f142b4b7
Zan-Shivam <shivam.zanjurne@introvertish.life>
2026-08-26 20:26:10 +0530

    Student-Roll-No Program

--- Assignments/Logic Building/Day 5/Question_16/PatternTwo.java
+++ Assignments/Logic Building/Day 5/Question_16/PatternTwo.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class PatternTwo {
     public static void main(String[] args) {
         int rows = 5;
@@ -10,5 +12,18 @@ public class PatternTwo {
             }
             System.out.println();
         }
+
+        int n=5;
+        for(int i=1; i<=n; i++){
+			for(int j = 1; j <= i; j++){
+             System.out.print(j);
+             if(j < i)
+                System.out.print("*");
+		}
+        System.out.println();	
     }
+
+
+        }
+
 }
